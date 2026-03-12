0e00aaf0840f65c06e72d405184a6df18892df25
Gail <gailtabirao@gmail.com>
2026-03-12 17:18:26 +0800

    added dashboard

--- central_server.py
+++ central_server.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
 import shutil
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -17,3 +18,40 @@ async def upload_model(file: UploadFile = File(...)):
         shutil.copyfileobj(file.file, buffer)
 
     return {"message": f"{file.filename} uploaded successfully"}
+
+@app.get("/dashboard", response_class=HTMLResponse)
+def dashboard():
+
+    html = """
+    <html>
+    <head>
+        <title>Federated Learning Dashboard</title>
+    </head>
+    <body>
+
+    <h1>Federated Learning Cardiovascular Prediction</h1>
+
+    <h2>System Architecture</h2>
+    <p>Central Server: Render</p>
+    <p>Hospital Nodes: Railway</p>
+
+    <h2>Model Performance</h2>
+
+    <ul>
+        <li>Main Model Accuracy: 0.73</li>
+        <li>Hospital 1 Model Accuracy: 0.75</li>
+        <li>Hospital 2 Model Accuracy: 0.76</li>
+        <li>Federated Model Accuracy: 0.78</li>
+    </ul>
+
+    <h2>Federated Workflow</h2>
+
+    <p>
+    Main Model → Hospital Training → Model Aggregation → Final Model
+    </p>
+
+    </body>
+    </html>
+    """
+
+    return html
