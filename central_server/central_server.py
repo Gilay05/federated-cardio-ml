784a30728a40a64061dbb9f8af0c4d91a3d76789
Gail <gailtabirao@gmail.com>
2026-03-12 19:52:09 +0800

    connect hospitals to central server

--- central_server/central_server.py
+++ central_server/central_server.py
@@ -1,46 +1,24 @@
-from fastapi import FastAPI, UploadFile, File
-import shutil
-
-app = FastAPI()
-
-
-@app.get("/")
-def home():
-    return {"message": "Federated Learning Central Server Running"}
-
-
-@app.post("/upload_model/")
-async def upload_model(file: UploadFile = File(...)):
-    save_path = f"models/{file.filename}"
-
-    with open(save_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-
-    return {"message": f"{file.filename} uploaded successfully"}
-
 from fastapi import FastAPI
 import requests
 
 app = FastAPI()
 
-HOSPITAL1_URL = "https://hospital1-production.up.railway.app/test_main_model"
-HOSPITAL2_URL = "https://hospital2-production.up.railway.app/test_main_model"
+HOSPITAL1_URL = "https://web-production-a4fbb.up.railway.app/test_main_model"
+HOSPITAL2_URL = "https://web-production-5e22a.up.railway.app/test_main_model"
 
+@app.get("/")
+def home():
+    return {"status": "Central server running"}
 
-@app.get("/performance")
-def get_performance():
+@app.get("/federated_performance")
+def federated_performance():
 
-    try:
-        h1 = requests.get(HOSPITAL1_URL).json()
-    except:
-        h1 = {"accuracy": "offline"}
-
-    try:
-        h2 = requests.get(HOSPITAL2_URL).json()
-    except:
-        h2 = {"accuracy": "offline"}
+    h1 = requests.get(HOSPITAL1_URL).json()
+    h2 = requests.get(HOSPITAL2_URL).json()
 
     return {
         "hospital1_accuracy": h1["accuracy"],
-        "hospital2_accuracy": h2["accuracy"]
+        "hospital2_accuracy": h2["accuracy"],
+        "hospital1_samples": h1["test_samples"],
+        "hospital2_samples": h2["test_samples"]
     }
